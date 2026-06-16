ec70915f74430d6e7502a1f7fcc89f7a1d45e9f4
malik mohamed <malikmohamed11333@gmail.com>
2026-06-16 04:47:42 +0300

    Add files via upload

--- scripts/player_control.gd
+++ scripts/player_control.gd
@@ -3,9 +3,11 @@ extends CharacterBody2D
 const SPEED = 128.0
 const JUMP_VELOCITY = -314.0
 
-var perform_jump : int
+var perform_jump : bool
 
 var health := 100
+var coin : int
+@export var coin_label : Label
 
 func _physics_process(delta):
 	var direction := Input.get_axis("ui_left", "ui_right")
@@ -16,21 +18,22 @@ func _physics_process(delta):
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		if is_on_floor() : $Body.play("idle")
-	
+
+	if Input.is_action_just_pressed("ui_accept") and perform_jump:
+		velocity.y = JUMP_VELOCITY
+		perform_jump = false
+		$Body.scale = Vector2(0.5, 1.5)
+		$"/root/JumpSfx".play()
+		
 	if is_on_floor():
-		perform_jump = 2
-		$Body.scale = Vector2(1, 1)
+		perform_jump = true
 	else:
 		velocity += get_gravity() * delta
 		$Body.play("jump")
 		$Body.scale = lerp($Body.scale, Vector2(1, 1), 0.1)
 	
-	if Input.is_action_just_pressed("ui_accept") and perform_jump:
-		velocity.y = JUMP_VELOCITY
-		perform_jump -= 1
-		$Body.scale = Vector2(0.5, 1.5)
-		$"/root/JumpSfx".play()
-	
 	move_and_slide()
 	
-	if position.y >= 191 : get_tree().reload_current_scene()
+	if position.y >= ProjectSettings.get_setting("display/window/size/viewport_height") : get_tree().reload_current_scene()
+	
+	coin_label.text = str(coin)
